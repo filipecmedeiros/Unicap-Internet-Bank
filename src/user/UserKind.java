0b016be4913e9c3c127dc01be29a2b78c9c72ecb
Gabriel Dantas <marcusgabriel.ds@gmail.com>
2019-05-01 21:14:29 -0300

    Simplify UserKind

--- src/user/UserKind.java
+++ src/user/UserKind.java
@@ -2,17 +2,7 @@ package user;
 
 
 public enum UserKind {
-    STANDARD (0),
-    VIP (1),
-    EXECUTIVO (2);
-
-    private int kind;
-
-    UserKind(int kind) {
-        this.kind = kind;
-    }
-
-    public int getKind() {
-        return this.kind;
-    }
+    STANDARD,
+    VIP,
+    EXECUTIVO;
 }
